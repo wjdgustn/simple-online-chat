79d2a53e3b52bb253266a6e3a183154a977fadd3
Hyunsoo, Chung <admin@hyonsu.com>
2020-05-26 18:08:14 +0900

    Fix typo, add chat command

--- main.js
+++ main.js
@@ -45,15 +45,27 @@ ws.on('connection', (socket) => {
     const req = socket.request;
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     ws.emit('news', { "type":"notice" , "chat" : "새로운 유저가 들어왔습니다!" });
+    console.log(`${ip}가 접속하였습니다.`);
     socket.on('disconnect', () => {
         ws.emit('news', { "type":"notice" , "chat" : "누군가 나갔습니다!" });
+        console.log(`${ip}가 접속을 해제하였습니다.`);
     });
     socket.on('news', (data) => {
-       if(data.type == "chat") {
-           ws.emit('news', data);
-           console.log("send");
+        if(data.type == "chat") {
+            if(data.chat.startsWith('/')) {
+               switch(data.chat.replace('/', '')) {
+                   case 'clear':
+                       socket.emit('news', { "type" : "action" , "value" : "clear_chat" });
+                       break;
+                   default:
+                       socket.emit('news', { "type" : "notice" , "chat" : "명령어 목록<br>/clear : 채팅창을 비웁니다." });
+               }
+           }
+           else {
+               ws.emit('news', data);
+           }
        }
-       console.log("Client Socket MEssage : " + JSON.stringify(data));
+       console.log("Client Socket Message : " + JSON.stringify(data));
     });
     socket.on('error', (error) => {
         console.error(error);
--- public/script.js
+++ public/script.js
@@ -5,12 +5,21 @@ var socket = io.connect(`${location.protocol}//${location.host}`, {
 socket.on('news', function (data) {
     var chatbox = document.getElementById("chatbox");
     if(data.type == "chat") {
-        chatbox.innerHTML = `${chatbox.innerHTML}<p class="normal_chat"><xmp>${data.username} : ${data.chat}</xmp></p>`
+        chatbox.innerHTML = `${chatbox.innerHTML}<xmp class="normal_chat">${data.username} : ${data.chat}</xmp>`
         chatbox.scrollTo(0, chatbox.scrollHeight);
     }
     if(data.type == "notice") {
         chatbox.innerHTML = `${chatbox.innerHTML}<p class="notice">${data.chat}</p>`
         chatbox.scrollTo(0, chatbox.scrollHeight);
+    }
+    if(data.type == "action") {
+        switch(data.value) {
+            case 'clear_chat':
+                document.getElementById('chatbox').innerHTML = '채팅창을 비웠습니다!';
+                break;
+            default:
+                console.error("undefined action");
+        }
     }
     console.log("서버 소켓 메시지 : " + JSON.stringify(data));
 });
